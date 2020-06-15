61b80b7967bdf0a0bf1e0a85b06d074a0b11ec63
maasa333 <konomaasa@gmail.com>
2020-06-14 17:39:30 -0700

    Add basic moodReducer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  componentDidMount() {
-    return fetch('http://localhost:3001/api/v1/moods')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
+  // componentDidMount() {
+  //   return fetch('http://localhost:3001/api/v1/moods')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }
 
   render() {
     return (
---
+++ src/reducers/moodReducer.js
@@ -0,0 +1,3 @@
+export default function moodReducer( state = { moodStates: [] }, action) {
+    return state
+}
