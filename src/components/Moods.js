f4de0fc6f6047e1471ef49549652d5ffae780389
maasa333 <konomaasa@gmail.com>
2020-07-03 11:21:22 -0700

    Mood images rendering on cards

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -2,7 +2,15 @@ import React from 'react';
 import {Container, Card, CardColumns} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
-import happy from '../images/happy.png';
+import Happy from '../images/Happy.png';
+import Anxious from '../images/Anxious.png';
+import Angry from '../images/Angry.png';
+import Humorous from '../images/Humorous.png';
+import Calm from '../images/Calm.png';
+import Fearful from '../images/Fearful.png';
+import Hopeful from '../images/Hopeful.png';
+import Sad from '../images/Sad.png';
+import Grateful from '../images/Grateful.png';
 
 const Moods = props => {
 
@@ -12,10 +20,9 @@ const Moods = props => {
             <CardColumns className='text-center'>
                 {props.moods.map(mood => 
                     <Card key={mood.id} border='primary' style={{ width: '20rem'}} >
-                        <Card.Img variant='top' src={happy} alt='happy' />
+                        <Card.Img variant='top' src={require(`../images/${mood.feeling}.png`)} />
                         <Card.Body className='text-center' >
                             <Card.Link href={`/moods/${mood.id}`} ><h5>{mood.feeling}</h5></Card.Link>    
-                            {/* <Link to={`/moods/${mood.id}`} >{mood.feeling}</Link>     */}
                         </Card.Body>
                     </Card>
                 )}
