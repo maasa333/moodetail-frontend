d82141bc179f6fc9f71a47103140a9ee2a67a446
maasa333 <konomaasa@fastmail.com>
2020-07-08 14:40:47 -0700

    Resolve case sensitive image file name issue in Moods.js file

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -8,9 +8,9 @@ const Moods = props => {
             <CardColumns className='text-center'>
                 {props.moods.map(mood => 
                     <Card key={mood.id} border='primary' style={{width: '20rem'}} >
-                        <Card.Img variant='top' src={require(`../images/${mood.feeling}.png`)} />
+                        <Card.Img variant='top' src={require(`../images/${mood.feeling.toLowerCase()}.png`)} />
                         <Card.Body className='text-center' >
-                            <Card.Link href={`/moods/${mood.feeling.toLowerCase()}`} ><h5>{mood.feeling}</h5></Card.Link>
+                            <Card.Link href={`/moods/${mood.feeling}`} ><h5>{mood.feeling}</h5></Card.Link>
                         </Card.Body>
                     </Card>
                 )}
@@ -20,19 +20,3 @@ const Moods = props => {
 }
 
 export default Moods;
-
-
-// Trying to resolve issue with case sensitive png file names in images:
-
-// const initialCap = (word) => {
-//     return word.charAt(0).toUpperCase() + word.slice(1)
-// }
-
-// const moodCards = props.moods.map(mood => 
-//     <Card key={mood.id} border='primary' style={{width: '20rem'}} >
-//         <Card.Img variant='top' src={require(`../images/${initialCap(mood.feeling)}.png`)} />
-//         <Card.Body className='text-center' >
-//             <Card.Link href={`/moods/${mood.feeling.toLowerCase()}`} ><h5>{initialCap(mood.feeling)}</h5></Card.Link>    
-//         </Card.Body>
-//     </Card>  
-// )
