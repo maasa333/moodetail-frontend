742073ad167223f75df1ddaf3c466673e558bc5d
Maasa <maasa.dev@gmail.com>
2020-09-20 15:41:23 -0700

    Add alternative button for prompts to either render as Notes or Try Clicking Me

--- src/containers/ActionsContainer.js
+++ src/containers/ActionsContainer.js
@@ -10,13 +10,21 @@ class ActionsContainer extends Component {
         props.deletePrompt(props.prompt.id, props.prompt.mood_id)
     }
 
+    clickMeButton = (props) => {
+        if (props.prompt.controllable === true) {
+            return <Button size='sm' variant='outline-primary' >Notes</Button>
+        } else {
+            return <Button size='sm' variant='outline-primary' >Try Clicking Me</Button>
+        }
+    }
+
     render() {
         return (
             <div>
                 <Accordion.Collapse>
                     <Card.Body>
                         <Button size='sm' variant='outline-danger' onClick={() => this.handleDelete(this.props)}>Delete</Button>
-                        
+                        {this.clickMeButton(this.props)}
                     </Card.Body>
                 </Accordion.Collapse>
             </div>
