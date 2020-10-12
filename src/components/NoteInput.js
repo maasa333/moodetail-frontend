48879da15686171888f40338678e10b485b248af
Maasa <maasa.dev@gmail.com>
2020-10-11 19:52:49 -0700

    Create NoteInput as a functional component with useState

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import {Form, Button} from 'react-bootstrap';
+import { connect } from 'react-redux';
+// import {addNote} from '../actions/addNote';
 
-class NoteInput extends Component {
-    render() {
-        return (
-            <div>
-                NoteInput component
-            </div>
-        );
-    }
+const NoteInput = () => {
+    const [input, setInput] = useState('');
+
+    console.log(input)
+    return (
+        <div>
+            <Form>
+                <Form.Control
+                    onChange={event => setInput(event.target.value)}
+                    placeholder='Enter note'>
+                </Form.Control>
+                <Button type='submit'>Submit</Button>
+            </Form>
+        </div>
+    );
 }
 
 export default NoteInput;
--- src/containers/NotesContainer.js
+++ src/containers/NotesContainer.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import {Form, Button, Card, Col, Row, Accordion} from 'react-bootstrap';
-import { connect } from 'react-redux';
-import {addNote} from '../actions/addNote';
 
 import NoteInput from '../components/NoteInput.js';
 import Notes from '../components/Notes.js';
@@ -9,7 +6,6 @@ import Notes from '../components/Notes.js';
 const NotesContainer = () => {
     return (
         <div>
-            NotesContainer
             <NoteInput />
             <Notes />
         </div>
